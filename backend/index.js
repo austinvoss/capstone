4211a1bd1a78b648b10edff1314ba6ba1b1853e1
Voss <vossaustin@gmail.com>
2023-09-21 17:04:11 -0700

    connected express server to postgres database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,25 @@
 const express = require("express");
+const { Client } = require("pg");
 
 const app = express();
+
+const client = new Client({
+  host: "localhost",
+  port: 5432,
+  user: "voss",
+  database: "voss",
+});
+
+async function connectDb() {
+  try {
+    await client.connect();
+    console.log("Connected to the database");
+  } catch (err) {
+    console.error("Failed to connect to the database", err);
+  }
+}
+
+connectDb();
 
 app.get("/ping", (req, res) => {
   res.send("pong");
